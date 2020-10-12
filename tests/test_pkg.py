4f6ce70d70bf5b83e71eb9208f8559ea0b2e1050
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2020-10-12 17:45:56 +0200

    fixed and added tests

--- tests/test_pkg.py
+++ tests/test_pkg.py
@@ -18,6 +18,36 @@ def test_Entry():
 
 def test_preprocess_form_comment():
     c = ds()
-    a, b = c.preprocess_form_comment(' ‰abc (T.) ', 'phonemic', 160, 'com', 1)
+    a, b = c.preprocess_form_comment(' ‰abc (T.) ', 'phonemic', '160', 'com', '1')
     assert a == 'äabc'
     assert b == 'com (T.)'
+
+    a, b = c.preprocess_form_comment('abc?', 'phonemic', '160', 'com', '17-610')
+    assert a == 'abc'
+
+    a, b = c.preprocess_form_comment('abc|', 'phonemic', '704', 'com', '1')
+    assert a == 'abcǀ'
+
+    a, b = c.preprocess_form_comment('abc T+', 'phonemic', '832', 'com', '1')
+    assert a == 'abc'
+
+    a, b = c.preprocess_form_comment('abc +T', 'phonemic', '832', 'com', '1')
+    assert a == 'abc'
+
+    a, b = c.preprocess_form_comment('abc R?', 'phonemic', '832', 'com', '1')
+    assert a == 'abc'
+
+    a, b = c.preprocess_form_comment('abc.???', 'phonemic', '831', 'com', '1')
+    assert a == 'abc.#'
+
+    a, b = c.preprocess_form_comment('PPN abc', 'phonemic', '234', 'com', '1')
+    assert a == 'abc'
+    assert b == 'PPN; com'
+
+    a, b = c.preprocess_form_comment('PEP abc', 'phonemic', '234', 'com', '1')
+    assert a == 'abc'
+    assert b == 'PEP; com'
+
+    a, b = c.preprocess_form_comment('abc [def]', 'phonemic', '168', '', '1')
+    assert a == 'abc'
+    assert b == '[def]'
